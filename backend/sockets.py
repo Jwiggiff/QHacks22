67a533c0fe925817e02017295f7d66c36bc1c205
Krishaan <Krishaan@Krishaans-Air>
2022-01-29 23:16:43 -0500

    added docstrings and comments

--- backend/sockets.py
+++ backend/sockets.py
@@ -4,19 +4,21 @@ import json
 import secrets
 from transcribe_text import transcribe
 
+# global variable defining the join dictionary
 JOIN = {}
 
 async def start(websocket, join_key):
     """
-    Handles a connection from the initial audio file.
- 
+    This function handles a connection from the initial audio file.
+    Parameters: websocket - an object representing the websocket, join_key - a string representing the room ID
+    Return: None
     """
+    # this stores the websocket inside a new dictionary
     connected = {websocket}
-    # join_key = secrets.token_urlsafe(12)
     JOIN[join_key] = connected
+    
+    # stores join key in a dictionary called event, deletes when exception is raised.
     try:
-        # Send the secret access tokens to the browser of the first player,
-        # where they'll be used for building "join" and "watch" links.
         event = {
                 "type": "init",
                 "join": join_key,}
@@ -27,25 +29,34 @@ async def start(websocket, join_key):
 
 async def join(websocket, join_key):
     """
-    Handles a connection from a second audio file
+    This function handles a connection from consecutive audio files
+    Parameters: websocket - an object representing the websocket, join_key - a string representing the room ID
+    Return: None
     """
+    # stores the join key in the global dictionary except if the key doesn't exist
     try:
         connected = JOIN[join_key]
     except KeyError:
         return
-    # Register to receive moves from this game.
+    # adds websocket to the global dictionary, removes if false
     try:
         connected.add(websocket)
     except:
         connected.remove(websocket)
 
 
-async def handler(websocket, path):
+async def handler(websocket):
+    """
+    This function handles a connection from consecutive audio files
+    Parameters: websocket - an object representing the websocket
+    Return: None
+    """
+    # ensures that the handler always sends and receives until while loop ends
     while True:
+        # starts, transcribes, and joins the websocket unless the connection is closed
         try: 
             message = await websocket.recv()
             event = json.loads(message)
-            # print(message)
             if event['type'] == 'recording':
                 print("recording received")
                 connected = JOIN[event['id']]
@@ -63,9 +74,14 @@ async def handler(websocket, path):
             break
 
 async def main():
+    """
+    This function controls the flow of execution.
+    Parameters: None
+    Return: None
+    """
     async with websockets.serve(handler, "", 8001):
-        await asyncio.Future() # run forever
-
+        # runs forever
+        await asyncio.Future() 
 
 if __name__ == "__main__":
     asyncio.run(main()) 
